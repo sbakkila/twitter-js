82ba69f6d6397cab7990e3c347efd01b08d7bde0
Denise Agathocleous <denise_agathocleous@hotmail.com>
2017-03-07 11:49:17 -0500

    nunjucks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require( 'express');
 const chalk = require('chalk');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
@@ -8,11 +9,19 @@ app.listen(3000, function(){
   console.log('Yes, I am here! :D');
 });
 
-app.get('/', function(req, res){
-  middlewareLog(req, res);
-  res.send('I work in a browser!');
-  }
-)
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+  //boilerplate setup
+app.set('view engine', 'html'); 
+app.engine('html', nunjucks.render); 
+nunjucks.configure('views', {noCache: true});
 
 var middlewareLog = function(req, res) {
     var reqInfo = req.method + ' ' + req.path + ' ';
@@ -20,6 +29,22 @@ var middlewareLog = function(req, res) {
     console.log(reqInfo);
 }
 
-app.use('*/special/*', function(req, res){
-  res.send('You have reached the special area!');
+
+app.use('/special/', function(req, res){
+	res.send('You have reached the special area!');
 });
+
+app.get('/', function(req, res){
+  // res.render('index.html', locals);
+  res.render( 'index', {title: 'Hall of Fame', people: [
+        { name: 'Sam'},
+        { name: 'Denise' },
+        { name: 'Fullstack'}
+    ]} );
+
+  middlewareLog(req, res);
+  // res.send('I work in a browser!');
+  }
+)
+
+
